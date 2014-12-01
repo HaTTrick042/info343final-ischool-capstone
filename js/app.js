5eb8f6b8b4b09b6899b8d7323567546001dd2562
Harpreet Singh <singhh17@uw.edu>
2014-11-30 16:05:20 -0800

    added filtering, my js file, extra css file, and a few tweaks to the index file

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 
 var capstonesUrl = 'https://api.parse.com/1/classes/capstones';
 
+
 angular.module('CapstonesList', [])
     .config(function($httpProvider) {
 
@@ -12,6 +13,7 @@ angular.module('CapstonesList', [])
     })
 
     .controller('CapstonesController', function($scope, $http) {
+
         //refreshes the list of comments by getting feed from parse.com
         $scope.refreshCapstones = function() {
             $http.get(capstonesUrl)
@@ -23,7 +25,44 @@ angular.module('CapstonesList', [])
             
         };
 
+        $scope.year="";
+        $scope.program="";
+        $scope.category = "";
+        
         $scope.refreshCapstones();
+
+
+        $scope.filter = function() {
+          
+            // $http.get(capstonesUrl + '?where={"year":year.value}')
+            //     .success(function(data) {
+            //         $scope.capstones = data.results;
+            //     });
+            $http.get(capstonesUrl)
+                .success(function(data) {
+                
+                    $scope.capstones = data.results.filter(function(capstone) {
+
+                        if ($scope.year == "yearAll" || $scope.year == "") {
+                            capstone.year = year.value;
+                        }
+
+                        if ($scope.category == "categoryAll" || $scope.category == "") {
+                            capstone.category = category.value;
+                        }
+
+                        if ($scope.program == "sortedAll" || $scope.program == "") {
+                            capstone.program = program.value;
+                        }
+
+                        return (capstone.year == year.value && capstone.category == category.value 
+                            && capstone.program == program.value);
+                    });
+
+                });
+
+        };
+
 
         $scope.addCapstones = function() {
             $scope.inserting = true;
@@ -40,24 +79,25 @@ angular.module('CapstonesList', [])
                     $scope.inserting = false;
                 });
         };
+
+        $scope.incrementLikes = function(capstone, amount) {
+            $scope.updating = true;
+            $http.put(capstonesUrl + '/' + capstone.objectId, {
+                likes: {
+                    __op: 'Increment',
+                    amount: amount
+                }
+            })
+            .success(function(responseData) {
+                capstone.likes = responseData.likes;
+            }).error(function(err) {
+                console.log(err);
+                })
+            .finally(function() {
+                $scope.updating = false;
+            });
+
+        };
+
     });
 
-var app = angular.module('myApp', []);
-
-function projectsController($scope, $http) {
-
-    $scope.projects = [];
-
-    $scope.loadProjects = function() {
-        var httpRequest = $http({
-            method: 'POST',
-            url: '/echo/json/',
-            data: mockDataForThisTest
-
-        }).success(function(data, status) {
-            $scope.projects = data;
-        });
-
-    };
-
-}
